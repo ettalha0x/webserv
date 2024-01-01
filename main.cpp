0a8a40a1d4cb2f20be75b1ff30ab2f5639d10a64
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-01-01 12:55:36 +0100

    adding main file to test sockets modules

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Networking/Networking.hpp"
+
+int main(){
+
+    std::cout << "Starting..." << std::endl;
+    std::cout << "Binding Socket..." << std::endl;
+    BindingSocket bs = BindingSocket(AF_INET, SOCK_STREAM, 0, 81, INADDR_ANY);
+    std::cout << "Listening Socket..." << std::endl;
+    ListeningSocket ls = ListeningSocket(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10);
+    std::cout << "Success!" << std::endl;
+    return 0;
+}
