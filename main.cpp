78db5fa362786f155439c2fec8685b8999fa4346
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-01-02 20:24:25 +0100

    adding tests for connectingSocket object

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(){
     BindingSocket bs = BindingSocket(AF_INET, SOCK_STREAM, 0, 81, INADDR_ANY);
     std::cout << "Listening Socket..." << std::endl;
     ListeningSocket ls = ListeningSocket(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10);
+    std::cout << "Connecting Socket..." << std::endl;
+    ConnectingSocket cs = ConnectingSocket(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY);
     std::cout << "Success!" << std::endl;
     return 0;
 }
