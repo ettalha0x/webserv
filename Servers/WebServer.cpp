ba652c43b37b897045fbf7c5975ce34fd541d995
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-01-11 13:30:35 +0100

    multiple clients connections seem to be working orrrrara

--- Servers/WebServer.cpp
+++ Servers/WebServer.cpp
@@ -28,18 +28,18 @@ void WebServer::responder() {
     response.addHeader("Date", getCurrentTimeInGMT());
 	std::string res = response.getHeaderString();
 	write(new_socket, res.c_str(), res.length());
-	std::string str = std::to_string(count++);
+	std::string str = std::to_string(count++) + "\n";
 	write(new_socket, str.c_str(), str.length());
 	close(new_socket);
 }
+
 void WebServer::launch() {
     fd_set  curr_socket, ready_socket;
-    int max_sockfd = 0;
+    int max_sockfd = get_sock()->get_socket();
 
     // init current socket
     FD_ZERO(&curr_socket);
     FD_SET(get_sock()->get_socket(), &curr_socket);
-    max_sockfd = get_sock()->get_socket();
     int j = 0;
 	while (true) {
         //save the current socket because select() is destructive
@@ -47,14 +47,14 @@ void WebServer::launch() {
 
         if ( j++ % 2 == 0) // this to avoid double printing the message bellow i don't fu** know why but it's working 
             std::cout << "------- WAITING FOR INCOMMING REQUSTES -------" << std::endl;
-        if (select(max_sockfd, &ready_socket, NULL, NULL, NULL) < 0) {
-            perror("select() error");
+        if (select(max_sockfd + 1, &ready_socket, NULL, NULL, NULL) < 0) {
+            perror("select()");
             exit(EXIT_FAILURE);
         }
 
-        for (int i=0; i <= max_sockfd; i++) {
-            if (FD_ISSET(i, &ready_socket)) {
-                if (i == get_sock()->get_socket()) {
+        for (int fd=0; fd < (max_sockfd + 1); fd++) {
+            if (FD_ISSET(fd, &ready_socket)) {
+                if (fd == get_sock()->get_socket()) {
                     accepter();
                     FD_SET(new_socket, &curr_socket);
                     if (new_socket > max_sockfd) {
@@ -62,8 +62,8 @@ void WebServer::launch() {
                     }
                 } else {
                     handler();
-                    FD_CLR(i, &curr_socket);
                     responder();
+                    FD_CLR(fd, &curr_socket);
 		            std::cout << "-------------- DONE --------------"<< std::endl;
                 }
             }
